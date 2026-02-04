802722ffbc40f8af8a8af295f483082876dfb386
gajju <gsingh.dba@gmail.com>
2026-02-04 13:59:33 +0530

    test file added

---
+++ test_pytestDemo.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+def test_printOne():
+    print("This is test One")
+
+def test_prinTwo():
+    print("This is test Two")
+    assert True,"test passed"
+
+def test_printThree():
+    assert True
